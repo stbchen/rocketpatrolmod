5ef06c37fc9c5af5915aa327b847ec9a859d319e
Jackson <deltarax@gmail.com>
2022-04-25 17:36:04 -0700

    Made some great changes

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -3,7 +3,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
         this.firing = false;
-        this.moveSpeed = 2;
+        this.moveSpeed = 12;
         this.sfxRocket = scene.sound.add('sfx_rocket')
     }
 
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -102,7 +102,7 @@ class Play extends Phaser.Scene {
         // 60-second play clock
         scoreConfig.fixedWidth = 0;
         this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
-            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME ROVER?', scoreConfig).setOrigin(0.5);
             this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or ← to Menu', scoreConfig).setOrigin(0.5);
             this.gameOver = true;
         }, null, this);
